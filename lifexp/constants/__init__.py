dea61cac7a535fdaf1a41e355fbc9417f88182fa
Sujith M <sujithm1610@gmail.com>
2023-09-29 23:54:59 +0530

    data ingestion

---
+++ lifexp/constants/__init__.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path(r"D:\model deploy end to end\lifeexpml\config\config.yaml")
+PARAMS_FILE_PATH = Path(r"D:\model deploy end to end\lifeexpml\params.yaml")
+SCHEMA_FILE_PATH = Path(r"D:\model deploy end to end\lifeexpml\schema.yaml")
