ac9e129a764df7239ece04c73f2cc4234655ec0a
Sujith M <sujithm1610@gmail.com>
2023-09-30 00:06:32 +0530

    data ingestion

---
+++ main.py
@@ -0,0 +1,15 @@
+from lifexp import logger
+from lifexp.pipeline.stage1_data_ingestion import DataIngestionTrainingPipeline
+
+
+
+STAGE_NAME = "Data Ingestion stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
